699b0320356401d8860faa44daa58b81de4c39e3
Sanjay Papaiahgari <sanjayrao609@gmail.com>
2024-06-13 17:48:09 +0000

    added new test code

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
-def add(x, y):
-    return x + y
+def toyou(x):
+    return f"hi {x}"
 
-# var=
-result = add(1, 2)
-print(f"This is the sum 1, 2, {result}")
+def add(x):
+    return x + 1
+
+def subtract(x):
+    return x - 1
